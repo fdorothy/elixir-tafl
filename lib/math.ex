42aff31df212f6003f66490203aa1215d86be61a
Fredric Dorothy <fredricdorothy@Fredrics-MacBook-Air.local>
2015-08-16 20:59:22 -0500

    untabified

--- lib/math.ex
+++ lib/math.ex
@@ -1,29 +1,28 @@
 defmodule Math do
-	def vfun([a|b], fun) when is_function(fun, 1) do
-		[fun.(a)|vfun(b,fun)]
-	end
-	def vfun([], fun) when is_function(fun, 1) do [] end
+  def vfun([a|b], fun) when is_function(fun, 1) do
+    [fun.(a)|vfun(b,fun)]
+  end
+  def vfun([], fun) when is_function(fun, 1) do [] end
 
-	def vfun([a|b], [c|d], fun) when is_function(fun, 2) do
-		[fun.(a,c)|vfun(b,d,fun)]
-	end
-	def vfun([], [], fun) when is_function(fun, 2) do [] end
+  def vfun([a|b], [c|d], fun) when is_function(fun, 2) do
+    [fun.(a,c)|vfun(b,d,fun)]
+  end
+  def vfun([], [], fun) when is_function(fun, 2) do [] end
 
 
-	def neg(a) do vfun(a, &(-&1)) end
-	def add(a, b) do vfun(a, b, &(&1+&2)) end
-	def sub(a, b) do vfun(a, b, &(&1-&2)) end
-	def dir(src, dst) do sub(dst,src) |> clamp(-1,1) end
+  def neg(a) do vfun(a, &(-&1)) end
+  def add(a, b) do vfun(a, b, &(&1+&2)) end
+  def sub(a, b) do vfun(a, b, &(&1-&2)) end
+  def dir(src, dst) do sub(dst,src) |> clamp(-1,1) end
 
-	def clamp(a, min, max) when is_list(a) do vfun(a, &clamp(&1, min, max)) end
-	def clamp(a, min, max) when is_integer(a) do
-		cond do
-			a < min -> min
-			a > max -> max
-			true -> a
-		end
-	end
+  def clamp(a, min, max) when is_list(a) do vfun(a, &clamp(&1, min, max)) end
+  def clamp(a, min, max) when is_integer(a) do
+    cond do
+      a < min -> min
+      a > max -> max
+      true -> a
+    end
+  end
 
-	def xor(a, b) do (a or b) and not (a and b) end
-
+  def xor(a, b) do (a or b) and not (a and b) end
 end
